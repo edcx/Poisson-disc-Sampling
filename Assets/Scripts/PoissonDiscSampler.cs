ef2033a5344b0d0eb650d216f76fa408e4485c1c
Engin Deniz Cengiz <engindenizce@gmail.com>
2016-12-16 00:42:15 +0300

    - Combined 2D and 3D versions

--- Assets/Scripts/PoissonDiscSampler.cs
+++ Assets/Scripts/PoissonDiscSampler.cs
@@ -36,7 +36,7 @@ public class PoissonDiscSampler : MonoBehaviour
     public Action SamplingFinished;
 
     void Start(){
-        StartSampling();
+        StartSampling(3);
         //StartSampling2D();
             
     }
@@ -45,18 +45,16 @@ public class PoissonDiscSampler : MonoBehaviour
 		 Vector3 result = v2 - v1;
 		 return result.sqrMagnitude;
 	 }
-	 public static float SquaredDistanceTo2D(Vector2 v1, Vector2 v2){
-		 Vector2 result = v2 - v1;
-		 return result.sqrMagnitude;
-	 }
 
      public Vector3[] GetPoints(){
          return grid;
      }
 
-    public void StartSampling ()
+    public void StartSampling (int targetDimensions)
 	{
-        dimensions = 3;
+        dimensions = targetDimensions;
+        if (dimensions == 2)
+            regionLength = 0;
         center = new Vector3(regionWidth / 2f, regionHeight / 2f, regionLength / 2f);
         minDistanceSquared = minDistance * minDistance;
 
@@ -65,9 +63,11 @@ public class PoissonDiscSampler : MonoBehaviour
         cols = Mathf.FloorToInt(regionWidth/cellSize);
         rows = Mathf.FloorToInt(regionHeight/cellSize);
         lens = Mathf.FloorToInt(regionLength/cellSize);
+        
+        if (dimensions == 2)
+            lens = 1;
 
         grid = new Vector3[ cols * rows * lens];
-        
 
         for (int i  = 0; i < grid.Length; i++)
 	        grid[i] = DummyValue;
@@ -78,13 +78,15 @@ public class PoissonDiscSampler : MonoBehaviour
         Vector3 point = new Vector3(
 	        point.x = Random.Range(regionWidth * 0.4f, regionWidth * 0.6f),
             point.y = Random.Range(regionHeight * 0.4f, regionHeight * 0.6f), 
-            point.z = Random.Range(regionLength * 0.4f, regionLength * 0.6f)
-            );
+            0);
+        if (dimensions == 3)
+            point.z = Random.Range(regionLength * 0.4f, regionLength * 0.6f); 
 
         int xIndex = Mathf.FloorToInt(point.x/regionWidth);
         int yIndex = Mathf.FloorToInt(point.y/regionHeight);
         int zIndex = Mathf.FloorToInt(point.z/regionLength);
-
+        if (dimensions == 2)
+            zIndex = 0;
         grid[xIndex + yIndex*cols + zIndex * cols * lens] = point;
 
         activeList = new List<Vector3>();
@@ -97,13 +99,9 @@ public class PoissonDiscSampler : MonoBehaviour
     IEnumerator Fill()
     {
         // ---------- Step 2 ----------
-        int count = 0;
         while (activeList.Count > 0)
         {
-
             yield return null;
-
-            count++;
 
             int randomIndex = Random.Range(0, activeList.Count);
             var randomPoint = activeList[randomIndex];
@@ -114,8 +112,10 @@ public class PoissonDiscSampler : MonoBehaviour
                 var length = Random.Range(minDistance, 2 * minDistance);
                 var angle = Random.Range(0, 360);
                 Vector3 sample = Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.up;
-                angle = Random.Range(0, 360);
-                sample = Quaternion.AngleAxis(angle, Vector3.up) * sample;
+                if (dimensions == 3){
+                    angle = Random.Range(0, 360);
+                    sample = Quaternion.AngleAxis(angle, Vector3.up) * sample;
+                }
                 sample = randomPoint + sample.normalized * length;
 
                 int col = Mathf.FloorToInt(sample.x/cellSize);
@@ -174,134 +174,11 @@ public class PoissonDiscSampler : MonoBehaviour
         
     }
 
-    public void StartSampling2D ()
-	{
-        dimensions = 2;
-        regionLength = 0;
-        center = new Vector3(regionWidth / 2f, regionHeight / 2f, regionLength / 2f);
-        minDistanceSquared = minDistance * minDistance;
-
-        // ---------- Step 0 ----------
-        cellSize = minDistance/Mathf.Sqrt(dimensions);
-        cols = Mathf.FloorToInt(regionWidth/cellSize);
-        rows = Mathf.FloorToInt(regionHeight/cellSize);
-
-        grid = new Vector3[ cols * rows];
-        
-
-        for (int i  = 0; i < grid.Length; i++)
-	        grid[i] = DummyValue;
-
-        Debug.Log("Grid Length: " + grid.Length);
-
-        // ---------- Step 1 ----------
-        Vector3 point = new Vector3(
-	        point.x = Random.Range(regionWidth * 0.4f, regionWidth * 0.6f),
-            point.y = Random.Range(regionWidth * 0.4f, regionWidth * 0.6f), 0);
-
-        int xIndex = Mathf.FloorToInt(point.x/regionWidth);
-        int yIndex = Mathf.FloorToInt(point.y/regionHeight);
-
-        grid[xIndex + yIndex*cols] = point;
-
-        activeList = new List<Vector3>();
-        activeList.Add(point);
-        pointCount++;
-        //Start Step 2
-        StartCoroutine(Fill2D());
-	}
-
-    IEnumerator Fill2D()
-    {
-        // ---------- Step 2 ----------
-        while (activeList.Count > 0)
-        {
-            yield return null;
-
-            int randomIndex = Random.Range(0, activeList.Count);
-            var randomPoint = activeList[randomIndex];
-
-            bool found = false;
-            for (int k = 0; k < rejectionLimit; k++)
-            {
-                var angle = Random.Range(0, 360);
-
-                var length = Random.Range(minDistance, 2 * minDistance);
-                Vector3 sample = Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.up;
-                sample = randomPoint + sample.normalized * length;
-
-                int col = Mathf.FloorToInt(sample.x/cellSize);
-                int row = Mathf.FloorToInt(sample.y/cellSize);
-
-                if (col < 0 || col >= cols || row < 0 || row >= rows) continue;
-                if (isSpherical){
-                    if (SquaredDistanceTo(center,sample) > (regionWidth / 2) * (regionWidth / 2))
-                        continue;
-                }
-
-                bool isOK = true;
-
-                for (int i = -1; i < 2; i++)
-                {
-                    for (int j = -1; j < 2; j++)
-                    {
-                        if ((col + i) < 0 || (col + i) >= cols || (row + j) < 0 || (row + j) >= rows) continue;
-                        if (grid[(col + i) + (row + j) * cols] == null) continue;
-
-                        Vector3 nPoint = grid[(col + i) + (row + j) * cols];
-                        var dSqred = SquaredDistanceTo(nPoint,sample);
-                        if (dSqred < minDistanceSquared)
-                        {
-                            isOK = false;
-                        }
-                    }
-                }
-                if (isOK)
-                {
-                    found = true;
-
-                    grid[col + row * cols] = sample;
-                    activeList.Add(sample);
-                    pointCount++;
-                    GameObject go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-
-                    go.transform.position = sample;
-                    go.transform.localScale = Vector3.one * minDistance * 0.5f;
-                    
-                    //break;
-                }
-            }
-            if (!found)
-                activeList.RemoveAt(randomIndex);
-        }
-        if (SamplingFinished != null)
-            SamplingFinished();
-        Debug.Log ("Point count: " + pointCount);
-    }
-
-    
-
     void OnDrawGizmos()
     {
         if (!Application.isPlaying || !drawGizmos)
             return;
         
-        /*//Draw Grid
-        Gizmos.color = Color.yellow;
-        if (dimensions == 2){
-            lens = 1;
-        }
-        for (int j = 0; j < lens; j++)
-        {
-            for (int i = 0; i < cols; i++)
-            {
-                Gizmos.DrawLine(new Vector3(cellSize * i,0, cellSize * j), new Vector3(cellSize * i, regionHeight, cellSize * j) );
-            }
-            for (int i = 0; i < rows; i++)
-            {
-                Gizmos.DrawLine(new Vector3(0, cellSize * i, cellSize * j), new Vector3(regionWidth, cellSize * i, cellSize * j));
-            }
-        }*/
         Gizmos.color = Color.green;
         if (isSpherical)
         {
@@ -315,21 +192,6 @@ public class PoissonDiscSampler : MonoBehaviour
             Gizmos.DrawLine(new Vector3(regionWidth,regionHeight, regionLength), new Vector3(regionWidth, 0, regionLength) );
             Gizmos.DrawLine(new Vector3(regionWidth,regionHeight, regionLength), new Vector3(regionWidth,regionHeight, 0) );
         }
-
-
-        /*Gizmos.color = Color.cyan;
-        for (int i = 0; i < grid.Length; i++)
-        {
-            if (grid[i] == null) continue;
-            Gizmos.DrawSphere(grid[i], minDistance * 0.26f);
-        }
-
-        Gizmos.color = Color.red;
-        for (int i = 0; i < activeList.Count; i++)
-        {
-            Gizmos.DrawSphere(activeList[i], minDistance * 0.26f);
-            
-        }*/
 
     }
 
